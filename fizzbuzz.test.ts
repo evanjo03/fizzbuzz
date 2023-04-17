f900d79e92df76ad20a1f95e6268d98cbbda19bb
John Evans <john@pop-os.localdomain>
2023-04-16 23:00:39 -0500

    Fix tests

--- fizzbuzz.test.ts
+++ fizzbuzz.test.ts
@@ -1,7 +1,7 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe("getFizzBuzzValueForNumber", () => {
-  test("should return Fizz if the number is divisible by 3", () => {
+  test("should call console log 100 times", () => {
     const spy = jest.spyOn(console, "log");
     fizzBuzz();
     expect(spy).toHaveBeenCalledTimes(100);
