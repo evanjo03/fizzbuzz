798144b085673e1b89dd1babaef9c6a77a3bc9d5
John Evans <john@pop-os.localdomain>
2023-04-16 23:01:43 -0500

    Fix tests p2

--- fizzbuzz.test.ts
+++ fizzbuzz.test.ts
@@ -2,7 +2,7 @@ import { fizzBuzz } from "./fizzbuzz";
 
 describe("getFizzBuzzValueForNumber", () => {
   test("should call console log 100 times", () => {
-    const spy = jest.spyOn(console, "log");
+    const spy = jest.spyOn(console, "log").mockImplementation(() => {});
     fizzBuzz();
     expect(spy).toHaveBeenCalledTimes(100);
   });
